509381e9b04eb99becdc83c97d1ccb2813cc2473
Juliette Cordor <grassojames5@gmail.com>
2022-08-16 12:36:29 +1000

    added target name string

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,17 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 
+const BIN_NAME: &str = {
+    cfg_if::cfg_if! {
+        if #[cfg(not(target_arch = "x86_64"))] {
+            compile_error!("Currently only x86_64 is supported");
+        } else if #[cfg(windows)] {
+            "youtube-dl.exe"
+        } else {
+            "youtube-dl"
+        }
+    }
+};
+
 fn main() {
     println!("Hello, world!");
 }
