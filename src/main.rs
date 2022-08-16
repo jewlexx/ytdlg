8f6d8f5ae9c9142140d4bd7467dea5e620909ea8
Juliette Cordor <grassojames5@gmail.com>
2022-08-16 12:40:51 +1000

    added bin_name and bin_download_url consts

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,23 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 
 const BIN_NAME: &str = {
-    cfg_if::cfg_if! {
-        if #[cfg(not(target_arch = "x86_64"))] {
-            compile_error!("Currently only x86_64 is supported");
-        } else if #[cfg(windows)] {
-            "youtube-dl.exe"
-        } else {
-            "youtube-dl"
+    const EXT: &str = {
+        cfg_if::cfg_if! {
+            if #[cfg(not(target_arch = "x86_64"))] {
+                compile_error!("Currently only x86_64 is supported");
+            } else if #[cfg(windows)] {
+                "youtube-dl.exe"
+            } else {
+                "youtube-dl"
+            }
         }
-    }
+    };
+
+    const_format::concatcp!("youtube-dl", EXT)
 };
+
+const BIN_DOWNLOAD_URL: &str =
+    const_format::concatcp!("https://youtube-dl.org/downloads/latest/", BIN_NAME);
 
 fn main() {
     println!("Hello, world!");
