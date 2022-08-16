fc8617053266f23d3607441dfe15fc1781d37722
Juliette Cordor <grassojames5@gmail.com>
2022-08-17 09:44:30 +1000

    fix

--- src/main.rs
+++ src/main.rs
@@ -53,12 +53,7 @@ fn dl_binary() {
 }
 
 fn main() {
-    human_panic::setup_panic!(Metadata {
-        authors: "Juliette Cordor",
-        name: "Youtube-DL Gui",
-        homepage: "https://github.com/jewlexx/ytdlg",
-        version: env!("CARGO_PKG_VERSION"),
-    });
+    human_panic::setup_panic!();
 
     dl_binary();
 
