100368d65379b498b449590149dcf6ebc626da20
Juliette Cordor <grassojames5@gmail.com>
2022-08-16 12:42:55 +1000

    fix

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 
+use std::fs::File;
+
 const BIN_NAME: &str = {
     const EXT: &str = {
         cfg_if::cfg_if! {
@@ -20,5 +22,11 @@ const BIN_DOWNLOAD_URL: &str =
     const_format::concatcp!("https://youtube-dl.org/downloads/latest/", BIN_NAME);
 
 fn main() {
+    let mut target_file = File::create(BIN_NAME).unwrap();
+
+    reqwest::blocking::get(BIN_DOWNLOAD_URL)
+        .unwrap().copy_to(&mut target_file)
+        .unwrap()
+
     println!("Hello, world!");
 }
