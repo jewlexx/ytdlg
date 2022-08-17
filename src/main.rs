7232053db9d07fb7a404134be5d96d9d7c5d1a39
Juliette Cordor <grassojames5@gmail.com>
2022-08-17 13:17:21 +1000

    fix

--- src/main.rs
+++ src/main.rs
@@ -137,6 +137,12 @@ impl DownloadStatus {
 
 static BIN_DOWNLOAD: Mutex<DownloadStatus> = Mutex::new(DownloadStatus(0, 1));
 
+trait LockMap {
+    fn map<F, T>(&self, f: F) -> T
+    where
+        F: FnOnce(&mut DownloadStatus) -> T;
+}
+
 impl eframe::App for Application {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
