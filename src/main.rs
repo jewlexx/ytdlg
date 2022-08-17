eb3409cd03861eaad0c4276a08a2273a82917b1d
Juliette Cordor <grassojames5@gmail.com>
2022-08-17 10:04:14 +1000

    fix

--- src/main.rs
+++ src/main.rs
@@ -59,7 +59,7 @@ fn main() {
         let mut msg = String::from("An error occurred:\n");
 
         if cfg!(debug_assertions) {
-            msg.push_str(&format!("{}\n", info.location().unwrap().to_string()));
+            msg.push_str(&format!("{}\n", info.location().unwrap()));
 
             eprintln!("{}", info);
         }
